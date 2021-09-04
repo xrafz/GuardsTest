2ecc6588c77e1fc85f13db7a9ff8d52dcae588f5
FuVy <amigo1044@yandex.ru>
2021-09-04 06:26:35 +0300

    Highlight selected creature

--- Assets/Scripts/UI/UnitSelector.cs
+++ Assets/Scripts/UI/UnitSelector.cs
@@ -1,17 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class UnitSelector : MonoBehaviour
 {
     [SerializeField]
     private TextOutput _hp, _damage, _attackRange;
 
+    [SerializeField]
+    private GameObject _highlight;
+
+    [SerializeField]
+    private Transform _highlightTransform;
+
+    private void Start()
+    {
+        Deselect();
+    }
+
     public void Select(Creature creature)
     {
         if (creature != null)
         {
             gameObject.SetActive(true);
+            _highlight.SetActive(true);
+            _highlightTransform.position = creature.Transform.position;
+
             _hp.Output(creature.Health.Current.ToString());
             _damage.Output(creature.Data.Damage.ToString());
             _attackRange.Output(creature.Data.AttackRange.ToString());
@@ -25,5 +38,6 @@ public class UnitSelector : MonoBehaviour
     public void Deselect()
     {
         gameObject.SetActive(false);
+        _highlight.SetActive(false);
     }
 }
